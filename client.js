280f79e25c42e69cd95961729996959b8d54a0a8
Marcus Vinicus <44820490+mvbrazz@users.noreply.github.com>
2021-11-29 20:44:58 -0300

    Refatorando client.js

--- client.js
+++ client.js
@@ -1,64 +1,46 @@
 const axios = require('axios').default;
 
-axios.post('http://localhost:3000/books', {
-    ID: 3,
-    name: 'My Boook',
-    author: 'Me and only me'
-})
-    .then((res) => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err.response.data);
-    });
 
-axios.post('http://localhost:3000/books', {
+async function main(){
+    
+    await axios.post('http://localhost:3000/books', {
+        ID: 3,
+        name: 'My Boook',
+        author: 'Me and only me'
+    })
+    
+    await axios.post('http://localhost:3000/books', {
     ID: 4,
     name: 'As aventuras de Marcus',
     author: 'Braz. Marcus'
-})
-    .then((res) => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err.response.data);
-    });   
-    
-axios.post('http://localhost:3000/books', {
+    });
+
+    await axios.post('http://localhost:3000/books', {
     ID: 4,
     name: 'O céu é azul escuro',
     author: 'Braz. Marcus'
-})
-    .then((res) => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err.response.data);
-    });     
-
-axios.get('http://localhost:3000/books')
-    .then((response) => {
-        console.log(response.data);
     });
 
-axios.get('http://localhost:3000/books/1')
-    .then((response) => {
-        console.log(response.data);
-    });  
-axios.get('http://localhost:3000/books/2')
-    .then((response) => {
-        console.log(response.data);
-    });
-axios.get('http://localhost:3000/books/4')
-    .then((response) => {
-        console.log(response.data);
-    });
-axios.get('http://localhost:3000/books/3')
-    .then((response) => {
-        console.log(response.data);
-    });  
-axios.get('http://localhost:3000/log')
-    .then((response) => {
-        console.log(response.data);
-    });                
+}
+
+main();
+
+async function main2(){
+    const aux = await axios.get('http://localhost:3000/books');
+    console.log(aux.data);
+
+    const aux1 = await axios.get('http://localhost:3000/books/1');
+    console.log(aux1.data);
+    const aux2 = await axios.get('http://localhost:3000/books/2');
+    console.log(aux2.data);
+    const aux3 = await axios.get('http://localhost:3000/books/4');
+    console.log(aux3.data);
+    const aux4 = await axios.get('http://localhost:3000/books/3');
+    console.log(aux4.data);
+    const aux5 = await axios.get('http://localhost:3000/log');
+    console.log(aux5.data);
+}
+main2();
+    
+    
 
